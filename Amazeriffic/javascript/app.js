f50d5441c1e370cb687d8e435dc17c4cf8e3e79d
johnnyflame <johnnyflamelee@gmail.com>
2020-09-22 20:27:34 +1200

    reshaping Amazerific into a todo list. Added tabs and js to show/hide content

---
+++ Amazeriffic/javascript/app.js
@@ -0,0 +1,14 @@
+var main = function() {
+    "use strict";
+
+    $(".tabs span").toArray().forEach(function(element) {
+        $(element).on("click", function() {
+            $(".tabs span").removeClass("active");
+            $(element).addClass("active");
+            $("main .content").empty();
+            return false;
+        });
+    });
+};
+
+$(document).ready(main);
